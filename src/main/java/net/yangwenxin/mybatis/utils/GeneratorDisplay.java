3568b9aedb256164103d75d3e6e8cce1a3d7def9
yangwenxin45 <yangwenxin45@gmail.com>
2020-05-08 03:41:08 +0800

    Mybatis-generator集成MyMapper插件

---
+++ src/main/java/net/yangwenxin/mybatis/utils/GeneratorDisplay.java
@@ -0,0 +1,32 @@
+package net.yangwenxin.mybatis.utils;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GeneratorDisplay {
+
+	public static void main(String[] args) throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config =
+				cp.parseConfiguration(getResourceAsStream("generatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+		for (String warning : warnings) {
+			System.out.println(warning);
+		}
+	}
+
+	public static InputStream getResourceAsStream(String path) {
+		return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+	}
+}
